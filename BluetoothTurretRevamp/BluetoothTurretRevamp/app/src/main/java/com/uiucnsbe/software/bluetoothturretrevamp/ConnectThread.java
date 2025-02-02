037809863407f0a1833ad32793b7e995a17fd59b
dokaf3 <148916821+dokaf3@users.noreply.github.com>
2025-02-01 19:19:11 -0600

    Deleted unnecessary subfolders and projects

--- BluetoothTurretRevamp/BluetoothTurretRevamp/app/src/main/java/com/uiucnsbe/software/bluetoothturretrevamp/ConnectThread.java
+++
@@ -1,7 +0,0 @@
-package com.uiucnsbe.software.bluetoothturretrevamp;
-
-import java.util.logging.Handler;
-
-public class ConnectThread {
-
-}
--- BluetoothTurretRevamp/BluetoothTurretRevamp/app/src/main/java/com/uiucnsbe/software/bluetoothturretrevamp/ConnectedThread.java
+++
@@ -1,4 +0,0 @@
-package com.uiucnsbe.software.bluetoothturretrevamp;
-
-public class ConnectedThread {
-}
